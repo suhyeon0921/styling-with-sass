2384e8022f9de6e244270bc0583f930aa7b9111f
suhyeonLee <suhyeon3484@naver.com>
2023-03-21 19:09:05 +0900

    버튼생성

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import './Button.scss';
+
+function Button({ children, size }) {
+  return <button className={['Button', size].join(' ')}>{children}</button>;
+}
+
+Button.defaultProps = {
+  size: 'medium',
+};
+
+export default Button;
