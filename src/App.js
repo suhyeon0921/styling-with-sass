5489f9b0361d9a10cc92019cd6660585a73a86e8
suhyeon <suhyeon3484@naver.com>
2023-03-21 20:00:33 +0900

    버튼 색상 설정

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import Button from './components/Button';
 function App() {
   return (
     <div className='App'>
+      {/* 버튼 사이즈 */}
       <div className='buttons'>
         <Button size='large'>BUTTON</Button>
         <Button>BUTTON</Button>
         <Button className='small'>BUTTON</Button>
+      </div>
+      {/* 버튼 색상 */}
+      <div className='buttons'>
+        <Button size='large' color='gray'>
+          BUTTON
+        </Button>
+        <Button color='gray'>BUTTON</Button>
+        <Button className='small' color='gray'>
+          BUTTON
+        </Button>
+      </div>
+      <div className='buttons'>
+        <Button size='large' color='pink'>
+          BUTTON
+        </Button>
+        <Button color='pink'>BUTTON</Button>
+        <Button className='small' color='pink'>
+          BUTTON
+        </Button>
       </div>
     </div>
   );
